fd324b0686d7b3368926e3c89a61b661340bd7f1
Diogo Regateiro <diogoregateiro@ua.pt>
2015-10-06 14:00:47 +0100

    Made the correct output and added a decision tree with bagging.

--- main.R
+++ main.R
@@ -1,12 +1,39 @@
-# Load RPART library
+# Install packages
+install.packages("ipred")
+
+# Load libraries
 library(rpart)
+library(ipred)
 
 # Load CSV
 data.train = read.csv('training.csv', TRUE, ';')
 data.test = read.csv('test.csv', TRUE, ';')
 
 # Simple Decision Tree
-model.dt <- rpart(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + X29 + X30, data = data.train, method="class")
+model.dt <- rpart(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + 
+				   X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + 
+                           X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + 
+                           X29 + X30, data = data.train, method="class")
 prediction.dt <- predict(model.dt,data.test,type="class")
 
-# TODO: Output in the correct format?
+out <- "dt.out"
+if (file.exists(out)) file.remove(out)
+write("Id,STATUS", file = out, append=TRUE)
+for(i in 1:length(prediction.dt)) {
+    write(paste(i, prediction.dt[i], sep = ","), file = out, append=TRUE)
+}
+
+# Decision Tree with Bagging
+model.bag_dt <- bagging(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + 
+                                 X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + 
+                                 X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + 
+                                 X29 + X30, data = data.train, coob=FALSE, method="class")
+
+prediction.bag_dt <- predict(model.bag_dt, newdata=data.test, type="class")
+
+out <- "bag_dt.out"
+if (file.exists(out)) file.remove(out)
+write("Id,STATUS", file = out, append=TRUE)
+for(i in 1:length(prediction.bag_dt)) {
+    write(paste(i, ifelse(prediction.bag_dt[i] >= 0, 1, -1), sep = ","), file = out, append=TRUE)
+}
