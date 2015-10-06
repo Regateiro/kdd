1322d7a08358c0d45ed5f610e23cbb94855074c1
Diogo Regateiro <diogoregateiro@ua.pt>
2015-10-06 17:46:30 +0100

    Corrected a bug in the boost_dt.R Added a boost_dt_adapt.R to change the boosting parameters and to try to find a better combination.

--- boost_dt_adapt.R
+++ boost_dt_adapt.R
@@ -6,23 +6,21 @@ library(adabag)
 
 # Load CSV
 data.train <- read.csv('training.csv', TRUE, ';')
-data.test <- read.csv('test.csv', TRUE, ';')
-
+#data.test <- read.csv('test.csv', TRUE, ';')
 data.train$STATUS <- as.factor(data.train$STATUS)
+data.test <- data.train[-(0:75),]
+data.train <- data.train[(0:75),]
+
 # predict expects the label to be present to build the confusion matrix... I just used ones
-data.test$STATUS <- as.factor(rep(1, 50))
+# data.test$STATUS <- as.factor(rep(1, 50))
+
 
 # Decision Tree with boosting
 model.boost_dt <- boosting(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + 
                                  X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + 
                                  X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + 
-                                 X29 + X30, data = data.train)
+                                 X29 + X30, data = data.train, coeflearn="Zhu")
 
 prediction.boost_dt <- predict(model.boost_dt, newdata=data.test, type="class")
 
-out <- "boost_dt.out"
-if (file.exists(out)) file.remove(out)
-write("Id,STATUS", file = out, append=TRUE)
-for(i in 1:length(prediction.boost_dt$class)) {
-    write(paste(i, prediction.boost_dt$class[i], sep = ","), file = out, append=TRUE)
-}
+print(prediction.boost_dt$confusion)
