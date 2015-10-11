ab0e78da0ca2137c2db1c3ec470c231b6994d754
Diogo José Domingues Regateiro <diogo.regateiro@gmail.com>
2015-10-11 21:55:41 +0100

    Forgot to save the files before committing. Oops.

--- boostsvm_ens_dt.R
+++ boostsvm_ens_dt.R
@@ -1,10 +1,10 @@
 # Install packages
 install.packages("adabag")
-install.packages("randomForest")
+install.packages("e1071")
 
 # Load libraries
 library(adabag)
-library(randomForest)
+library(e1071)
 
 # Load CSV
 data.train <- read.csv('training.csv', TRUE, ';')
@@ -18,7 +18,7 @@ data.test$STATUS <- as.factor(c(rep(1, 25),rep(-1,25)))
 model.boost_dt <- boosting(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + 
                                  X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + 
                                  X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + 
-                                 X29 + X30, data = data.train, coeflearn="Zhu")
+                                 X29 + X30, data = data.train)
 
 prediction.boost_dt <- predict(model.boost_dt, newdata=data.test, type="class")
 
@@ -31,17 +31,17 @@ model.svm <- svm(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 +
 prediction.svm <- predict(model.svm, newdata=data.test, probability=TRUE)
 
 # Ensemble
-prediction.boostforest_ens_dt.prob <- prediction.boost_dt$prob + attr(prediction.svm,"probabilities")
+prediction.boostsvm_ens_dt.prob <- prediction.boost_dt$prob*1.7 + attr(prediction.svm,"probabilities")*0.3
 
-prediction.boostforest_ens_dt <- vector(,length(prediction.boostforest_ens_dt.prob[,1]))
-for(i in 1:length(prediction.boostforest_ens_dt.prob[,1])) {
-    prediction.boostforest_ens_dt[i] <- ifelse(prediction.boostforest_ens_dt.prob[i,1] < prediction.boostforest_ens_dt.prob[i,2],1,-1)
+prediction.boostsvm_ens_dt <- vector(,length(prediction.boostsvm_ens_dt.prob[,1]))
+for(i in 1:length(prediction.boostsvm_ens_dt.prob[,1])) {
+    prediction.boostsvm_ens_dt[i] <- ifelse(prediction.boostsvm_ens_dt.prob[i,1] < prediction.boostsvm_ens_dt.prob[i,2],1,-1)
 }
 
 # Output
-out <- "boostforest_ens_dt.out"
+out <- "boostsvm_ens_dt.out"
 if (file.exists(out)) file.remove(out)
 write("Id,STATUS", file = out, append=TRUE)
-for(i in 1:length(prediction.boostforest_ens_dt)) {
-    write(paste(i, prediction.boostforest_ens_dt[i], sep = ","), file = out, append=TRUE)
+for(i in 1:length(prediction.boostsvm_ens_dt)) {
+    write(paste(i, prediction.boostsvm_ens_dt[i], sep = ","), file = out, append=TRUE)
 }
--- svm.R
+++ svm.R
@@ -16,13 +16,13 @@ data.test$STATUS <- as.factor(c(rep(1, 25),rep(-1,25)))
 model.svm <- svm(STATUS ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 + 
                                  X11 + X12 + X13 + X14 + X15 + X16 + X17 + X18 + X19 + 
                                  X20 + X21 + X22 + X23 + X24 + X25 + X26 + X27 + X28 + 
-                                 X29 + X30, data = data.train)
+                                 X29 + X30, data = data.train, probability=TRUE, kernel="radial")
 
 prediction.svm <- predict(model.svm, newdata=data.test, probability=TRUE)
 
-out <- "boost_dt.out"
+out <- "svm.out"
 if (file.exists(out)) file.remove(out)
 write("Id,STATUS", file = out, append=TRUE)
-for(i in 1:length(prediction.boost_dt$class)) {
-    write(paste(i, prediction.boost_dt$class[i], sep = ","), file = out, append=TRUE)
+for(i in 1:length(prediction.svm)) {
+    write(paste(i, prediction.svm[i], sep = ","), file = out, append=TRUE)
 }
